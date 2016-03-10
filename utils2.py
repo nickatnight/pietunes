486a8040c2d4259f39e1680190626977922d5005
nick <kay.muneee@gmail.com>
2016-03-09 19:29:48 -0800

    fixed mutagen to work with path

--- utils2.py
+++ utils2.py
@@ -1,5 +1,6 @@
 import os
-from eyed3.id3 import Tag
+from mutagen.easyid3 import EasyID3
+
 
 env = os.environ['HOME']
 
@@ -8,7 +9,6 @@ if 'nick' in env:
 else:
     music_dir = '/mnt/seagatehd/Music'
 
-tag = Tag()
 music_dict = {}
 
 def fetch_data():
@@ -17,19 +17,32 @@ def fetch_data():
         for name in files:
             if name.endswith('.mp3'):
                 full_path = os.path.join(root,name)
-                tag.parse(full_path)
+                audio = EasyID3(full_path)
+                #print audio['album'][0] || None
+                if not 'album' in audio:
+                    audio['album'] = 'No album title.'
+                    audio.save()
+                if not 'artist' in audio:
+                    audio['artist'] = 'No artist name.'
+                    audio.save()
+                if not 'title' in audio:
+                    audio['title'] = 'No title name.'
+                    audio.save()
                 entry = {}
                 entry[pk] = {
-                    'title': tag._getTitle(),
-                    'artist': tag._getArtist(),
-                    'album': tag._getAlbum(),
+                    'title': audio['title'][0],
+                    'artist': audio['artist'][0],
+                    'album': audio['album'][0],
                     'path': full_path.replace("/Users/nick/Music/iTunes/iTunes Media/Music/","static/music/"),
                     'pk': pk
                 }
                 pk += 1
                 music_dict.update(entry)
-                #tag.parse(full_path)
                 #print os.path.dirname(os.path.dirname(os.path.join(root, name)))
 
+
+
+
 fetch_data()
+print music_dict
 #print music_dict
