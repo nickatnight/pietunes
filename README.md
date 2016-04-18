d3ee27e6f16d5119805ced9a6e5c4eae27a3a22e
nickatnight <kay.muneee@gmail.com>
2016-04-18 11:49:01 -0700

    fixed search. README edit

---
+++ README.md
@@ -0,0 +1,11 @@
+# pieTunes  
+  
+  
+  
+Have a bunch of music you want to access from the cloud? Don't want to pay those
+ugly storage fees? If your like me and have a couple hundred gigs of audio
+files, then pieTunes is for you.
+
+## What you will need  
+- RaspberryPi
+- External HD (music only)
--- pitunes.py
+++ pitunes.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
-import re
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    #term = request.args.get('srch-term')
-    #if term:
-    #    print 'Term is not None'
-    #length = len(utils2.music_dir)
     c = map(chr, range(97, 123))
+    if request.method == 'POST':
+        term = request.form['srch_term']
+        es = Elasticsearch()
+        query = es.search(index="music", body={"size": 80, "query":
+                                                {"prefix":{"artist":term}}})
+        return render_template('search.html', music=query)
 
     return render_template('home.html', chars=c)
 
@@ -22,6 +23,11 @@ def index(title_letter):
     query = es.search(index="music", body={"size":80, "query":
                                            {"prefix":{"artist": title_letter}}})
     return render_template('index.html', music=query, chars=c)
+
+@app.route('/search')
+def search():
+    query = None
+    return render_template('search.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5666)
